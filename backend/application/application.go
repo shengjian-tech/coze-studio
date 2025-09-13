0b3374297a0f612a7576b946dea24095354e2066
guoyufeng <2207964216@qq.com>
2025-09-13 11:13:51 +0800

    导入textToImage

--- backend/application/application.go
+++ backend/application/application.go
@@ -20,7 +20,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/coze-dev/coze-studio/backend/application/publishThird"
-
+	"github.com/coze-dev/coze-studio/backend/application/vPlugin/textToImage"
 
 	"github.com/coze-dev/coze-studio/backend/application/openauth"
 	"github.com/coze-dev/coze-studio/backend/application/template"
